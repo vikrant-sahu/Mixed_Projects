ca0f4bdc5dde3fc1d8e622ef803cd777b9f9807a
Vikrant Sahu <vikrant.sahu40@gmail.com>
2020-06-04 15:48:24 +0530

    adding two py files

---
+++ user_info_analysis.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 13:54:24 2020
+
+@author: vikrant.sahu
+"""
+
+import pymongo
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+
+from pymongo import MongoClient
+
+import datetime as dt
+from dateutil.parser import parse
+
+from datetime import *
+from dfply import *
+
+
+#point the client at mongo URL
+client = MongoClient('192.168.100.180:27017', username='git_analytics',
+                                                password='dspg@007',)
+#select database
+db = client['Github-App-temp']
+
+#http://gitserver.digite.in/SALM/AI/automl/blob/master/com/utility/database/MongoDBUtils.py
+
+# =============================================================================
+# cursor = db.issues_info_all.find({
+#   "repo_id": "31792824" 
+#    ,"issue_created_at": { "$gt": (1582482600000), "$lt": (1584901799999)}
+#  })
+# print(list(cursor))
+# =============================================================================
+
+
+####Load all the collections######
+
+##user details
+user_details_list = db["user_info"]
+user_details_df = pd.DataFrame(list(user_details_list.find()))
+
+##user_issues
+user_issues_list = db["issues_user_bulk"]
+user_issues_df = pd.DataFrame(list(user_issues_list.find()))
+
+##user_pulls
+user_pulls_list = db["pulls_user_bulk"]
+user_pulls_df = pd.DataFrame(list(user_pulls_list.find()))
+
+##user_repos
+user_repos_list = db["repos_user_bulk"]
+user_repos_df = pd.DataFrame(list(user_repos_list.find()))
+
+##push_users
+user_pushs_list = db["push_user"]
+user_pushs_df = pd.DataFrame(list(user_pushs_list.find()))
+
+###Querry for last 4 weeks commits
+ 
+todays_date = dt.date.today()
+date_weekday = todays_date.weekday()
+
+start_date = todays_date - dt.timedelta(days=(21 + date_weekday))
+
+##datetime to epoch:
+start_date_ep = int(parse(start_date.strftime("%m/%d/%Y")).timestamp()*1000)
+
+start_date_ep
+end_date_ep
+## selected user_id = 551196
+###query
+query_cursor = db.issues_info_all.find({
+  "issuer_id": "551196"   #{"user_id": {$in : ["79138"]}}
+   ,"issue_created_at": { "$gt": (1583692200    000), "$lt": (1585593000000)}
+ })
+
+## epoch to datetime
+# =============================================================================
+# epoch = (1241006778000 / 1000)
+# dt = time.strftime("%d %m %Y %H:%M", time.gmtime(epoch))
+# 
+# =============================================================================
+
+
+##Operation on DFs to calcualte various metrics####
+
+##Get the max and min of user pulls
+max_pull_epoch = user_pulls_df.pull_created_at.max()
+min_pull_epoch = user_pulls_df.pull_created_at.min()
+
+
+datetime.utcfromtimestamp(max_pull_epoch/1000).strftime('%Y-%m-%d %H:%M:%S')
+datetime.utcfromtimestamp(min_pull_epoch/1000).strftime('%Y-%m-%d %H:%M:%S')
+1585409994000
+
+##Get the unique user for pulls, push and issues##
+user_issues_df >> mask(X.state == "closed", X.assignees_logins == "[]") >> select(X.assignees_logins)
+
+#>> select(X.assignees_logins)
+
+
+
+##Create a new collection
+temp_collection = db["temp_collection"]
+
+####push the dataframe to mongo ####
+import json
+
+###remove the _id created by mongo db
+df_to_push = user_details_df.drop("_id", axis=1)
+
+###convert the df to json
+records = json.loads(df_to_push.T.to_json()).values()
+
+##insert into mongo
+db.temp_collection.insert_many(records)
+
+###Essential mongoDB operations###
+
+##Querry to update the value
+collection = db['installation_info']
+        query = {"installation_id": str(df['installation.id'][0])}
+        collection.update_many(query,{'$set':{'installation_status':'deleted'}})
+
+##Querry to update the value
+collection = db['user_info']
+        query = {"user_id": "18552651"}
+        collection.update_many(query,{'$set':{'metric_calc_status':'done'}})
+
+##for a user id  and highest of week_start_date
+query_cursor = db.summarized_weekly.find({
+  "user_id": "79138" }).sort("week_start_date", -1).limit(1)
+
+##delete a record with condition
+collection = db['user_info']
+        query = {"user_id": "18552651", }
+        collection.delete_many(query)
+
+
+####add a column to a mongo collection#####
+##user details
+user_details_list = db["user_info"]
+user_details_df = pd.DataFrame(list(user_details_list.find()))
+
+user_details_df = user_details_df.drop(["_id"], axis = 1)
+
+##create two columns
+user_details_df["last_refresh"] = 0  
+user_details_df["abt_calculated"] = "NA"
+
+###convert the df to json
+import json
+records = json.loads(user_details_df.T.to_json()).values()
+
+##insert into mongo
+db.user_info.insert_many(records)
+
+
+
+
+###get user id for a user_name
+user_login = "jonahwilliams"
+
+collection = db['user_info']
+querry = {"user_name": user_login}
+cursor = collection.find(querry)
+user_id = pd.DataFrame(list(cursor))['user_id'][0]
+
+#Function to delte a list of folders
+def delete_repo(path_list):
+      
+      for path in path_list:
+            os.rmdir(path)
+            
+            
+installation_id = "7853227"
+
+collection = db['installation_info']
+querry = {"installation_id": installation_id}
+cursor = collection.find(querry)
+path_list = pd.DataFrame(list(cursor))['repo_cloned_path']
+
+delete_repo(path_list)
+
+for path in path_list:
+      print(path)
+      print(type(path))
+
+##String to date 
+ now_utc = datetime.strptime(now_utc, '%d-%m-%Y')
+ #https://www.tutorialspoint.com/How-do-I-convert-a-datetime-to-a-UTC-timestamp-in-Python
+#########
+      [day.date()
+                  for day in rrule(MONTHLY
+                     , dtstart = date(first_month_start.year,first_month_start.month,first_month_start.day)
+                     , until = last_month_end)]
+
+wk_list = pd.date_range(start= created_at_utc, end=now_utc, 
+                         freq='W-MON').strftime('%d-%m-%Y').tolist()
+
+#########
+pr_user >> select(X.additions, X.pull_merged_at, X.deletions) >> group_by(X.pull_merged_at) >> summarize_each(sum)
+
+cols = ["additions", "pull_merged_at"]
+summarise_opp = "n"
+pr_user >> select(X[cols]) >> group_by(X.pull_merged_at) >> summarize_each([eval(summarise_opp)], X.additions)
+
+
+##Test
+issuer_id = "4823303"
+user_id = "4823303"
+load_data(db,'issues_user_bulk',{'issuer_id':issuer_id})
+[s + mystring for s in mylist]
+
+# =============================================================================
+#             summarised_df = (df_subset >> group_by(X.grpBy_col) >> summarize_each([eval(summarise_opertn)]
+#                                                       , X[summarise_cols]  ))
+# =============================================================================
+# =============================================================================
+# #####testing
+# df = pr_user
+# time_freq = "week"
+# date_col = "pull_merged_at"
+# summarise_cols = ["commits", "changed_files"]
+# summarise_cols_names = ["total_commits", "files_modified"]
+# summarise_opertn = "np.sum"
+# cols_list = ["X." + s  for s in summarise_cols]
+# 
+# str(["X." + s  for s in summarise_cols])[1:-1]
+# 
+# eval(str(["X." + s  for s in summarise_cols])[1:-1])
+# 
+# map(eval, cols_list)
+# 
+# [eval(i) for i in cols_list]
+#             
+# X.commits, X.changed_files
+# 
+# ##########
+# xx = df_subset.groupby("grpBy_col").sum()[summarise_cols].reset_index()
+# 
+# summarised_df.rename(
+#     columns={i:j for i,j in zip(summarise_cols,summarise_cols_names)}, inplace=True
+# )
+# =============================================================================
+user_id = "10163662" #"4823303" 
+collection = db['summarized_monthly']
+querry = {"user_id": user_id}
+cursor = collection.find(querry)
+df_2 = pd.DataFrame(list(cursor))
+
+
+df_3 = load_data(db = db, collection_name = "installation_info", query = {})
---
+++ weekly_summarised_A.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 15:43:45 2020
+
+@author: amoolya.shetty
+"""
+import pymongo
+import pandas as pd
+import time as t
+from dfply import * 
+pd.set_option('display.max_columns', None)
+from pymongo import MongoClient
+from datetime import datetime
+import datetime as dt
+from dateutil.parser import parse
+
+
+
+#------------------------------------------------------------------------------------------------
+# function definition section
+
+# function to convert the epoch values to UTC date format
+def weekStartInUtc(utcDate):
+    utcDateWeekday = utcDate.weekday()
+    weekStart = utcDate - dt.timedelta(utcDateWeekday)
+    return(weekStart)
+    
+def weekEndInUtc(utcDate):
+     utcDateWeekday = utcDate.weekday()
+     weekEnd = utcDate + dt.timedelta(7-utcDateWeekday)
+     return(weekEnd)
+     
+# function to convert epoch to UTC 
+def convertEpochToUtc(epochTime):
+    dateString = datetime.utcfromtimestamp(epochTime/1000).strftime('%Y-%m-%d')
+    dateUtc = datetime.strptime(dateString, "%Y-%m-%d")
+    return (dateUtc)
+
+# function to convert UTC to Epoch
+def convertUtcToEpoch(utcTime):
+    return (int(parse(utcTime.strftime("%m/%d/%Y")).timestamp()*1000))
+
+# function to find intermediate weeks week start date in UTC
+def findIntermediateDurationStart(firstWeekStartEp,lastWeekEndEp,baseEpoch):
+    #baseEpoch for week is 604800000
+    intermediateWeekStartSeries = pd.Series (range(firstWeekStartEp,lastWeekEndEp,baseEpoch))
+    return(intermediateWeekStartSeries)
+    
+# function to calculate user Account Time Frame - create date up until current date
+def userTimeFrame(userId):
+    #extract a specific user info
+    specificUserInfo = userDataDf[userDataDf['user_id'] == userId];
+    # extract the date when the user account was created
+    createdAtEp = specificUserInfo['created_at'];
+    # convert epoch to UTC
+    createdAtUtc = convertEpochToUtc(createdAtEp);
+    # find the week's start date In UTC 
+    firstWeekStart = weekStartInUtc(createdAtUtc);
+    firstWeekStartEp = convertUtcToEpoch(firstWeekStart);  
+    # obtain current date
+    currentDateUtc = dt.date.today();
+    # find week's end date in UTC
+    lastWeekEndUtc = weekEndInUtc(currentDateUtc);
+    lastWeekEndEp = convertUtcToEpoch(lastWeekEndUtc);
+    timeFrame=[firstWeekStartEp,lastWeekEndEp];
+    return(timeFrame)
+ 
+#function to create a summarized collection for weeekly/monthly 
+def summarizedTable(userId,firstDurationStartEp,lastDurationEndEp,baseEpoch):
+    columnNames = ['user_id','week_start_epoch'];
+    #create a df that holds the week start epoch values from start to current date
+    intermediateWeekStart = findIntermediateDurationStart(firstDurationStartEp,lastDurationEndEp,baseEpoch);
+    # Create a new df and populate the collection with user id and start and end epoch values
+    weeklySummarizedDf = pd.DataFrame(columns = columnNames);
+    weeklySummarizedDf['week_start_epoch'] = intermediateWeekStart ;
+    weeklySummarizedDf['user_id'] = 'userId' ; #replace with the userId
+    weeklySummarizedDf['week_start_utc'] = weeklySummarizedDf['week_start_epoch'] .apply(convertEpochToUtc)
+    return (weeklySummarizedDf)
+#------------------------------------------------------------------------------------------------
+#function to perform data filteration on any collection for a given user TimeFrame
+    specificUserPullInfo = collectionDf[collectionDf[collectionUserFieldName] == userId];
+    specificUserPullInfo['count'] = 1;
+    # if pull_merged _at is NAN,replace it with 0
+    specificUserPullInfo[operationFieldName] = specificUserPullInfo[operationFieldName].fillna(0);
+    filteredUserData = specificUserPullInfo[specificUserPullInfo[operationFieldName]>firstDurationStartEp ]
+    filteredUserData['time_utc'] = filteredUserData[operationFieldName].apply(convertEpochToUtc);
+    filteredUserData['week_start_utc'] = filteredUserData['time_utc'].apply(weekStartInUtc);
+#    groupByResultDf = (filteredUserData >>
+#        group_by(X.week_start_utc)>>
+#         summarize(totalCommits = X.commits.sum())
+#      )
+    return (filteredUserData)
+
+#--------------------------------------------------------------------------------------------------
+
+#point the client at mongo URI
+client = MongoClient('192.168.100.180:27017', username='git_analytics',
+                                                password='dspg@007',)
+#select database Github-App-temp from mongoDb
+#client.list_database_names()
+db = client['Github-App-temp']
+
+#select collection that holds user info
+userData =  db['user_info']
+userDataDf = pd.DataFrame(list(userData.find()))
+
+#select collection that holds user user pull information
+pullsUserBulk =  db['pulls_user_bulk']
+pullsUserBulkDf = pd.DataFrame(list(pullsUserBulk.find()));
+
+#select collection that holds user user pull information
+issuesUserBulk =  db['issues_user_bulk']
+issuesUserBulkDf = pd.DataFrame(list(issuesUserBulk.find()));
+
+#for col in issuesUserBulkDf.columns:
+#    print(col)
+
+
+# pull required info for a specific user
+userId= "79138";
+# baseEpoch is epach value for a a given duration i.e weekly or monthy
+# baseEpoch for a week is 604800000
+baseEpoch = 604800000 ;
+
+# filter out the start(account created) and end time (current date) for a user
+timeFrame  = userTimeFrame(userId);
+firstDurationStartEp = timeFrame[0]
+lastDurationEndEp = timeFrame[1]
+#create a dataframe that holds weekly summarized information
+weeklySummarizedDf = summarizedTable(userId,firstDurationStartEp,lastDurationEndEp,baseEpoch);
+
+# filter out information from a pull_user_bulk collection for a specific user
+collectionDf = pullsUserBulkDf ;
+collectionUserFieldName = 'pull_requester_id' ;
+operationFieldName = 'pull_merged_at';
+filteredUserData = tableOperation(collectionDf,collectionUserFieldName,userId,operationFieldName,firstDurationStartEp,lastDurationEndEp);
+#-----------------------------------------------------------------------------------------------------------------------------------
+# find total commits per week for a user
+groupByResultCommitsDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalCommits = X.commits.sum())
+      );
+
+result = pd.merge(weeklySummarizedDf, groupByResultCommitsDf, how='left',  on=['week_start_utc']);
+#------------------------------------------------------------------------------------------------------------
+# find the weeks that have number of additions made by the user above 300 LOC
+groupByResultAdditionsDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalMajorAdditions = X.additions.sum())
+      );
+groupByResultAdditionsDf['totalMajorAdditions'] = groupByResultAdditionsDf['totalMajorAdditions'].apply( lambda x : pd.to_numeric(x))
+groupByMajorAdditionDf= groupByResultAdditionsDf[groupByResultAdditionsDf ['totalMajorAdditions'] >300]
+result1 = pd.merge(result, groupByMajorAdditionDf, how='left',  on=['week_start_utc']);
+#------------------------------------------------------------------------------------------=------------
+# find the weeks that have number of additions made by the user below 300 LOC
+        group_by(X.week_start_utc)>>
+         summarize(totalMinorAdditions = X.additions.sum())
+      );
+groupByResultAdditionsDf['totalMinorAdditions'] = groupByResultAdditionsDf['totalMinorAdditions'].apply( lambda x : pd.to_numeric(x))
+groupByMinorAdditionDf= groupByResultAdditionsDf[groupByResultAdditionsDf ['totalMinorAdditions'] <300]
+result2 = pd.merge(result1, groupByMinorAdditionDf, how='left',  on=['week_start_utc']);
+
+#--------------------------------------------------------------------------------------
+# find the weeks that have number of deletions made by the user above 300 LOC
+groupByResultDeletionDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalMajorDeletions = X.deletions.sum())
+      );
+
+groupByResultDeletionDf['totalMajorDeletions'] = groupByResultDeletionDf['totalMajorDeletions'].apply( lambda x : pd.to_numeric(x))
+groupByMajorDeletionDf= groupByResultDeletionDf[groupByResultDeletionDf ['totalMajorDeletions'] >300]
+result3 = pd.merge(result2, groupByMajorDeletionDf, how='left',  on=['week_start_utc']);
+
+#-------------------------------------------------
+# find the weeks that have number of deletions made by the user below 300 LOC
+groupByResultDeletionDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalMinorDeletions = X.deletions.sum())
+      );
+
+groupByResultDeletionDf['totalMinorDeletions'] = groupByResultDeletionDf['totalMinorDeletions'].apply( lambda x : pd.to_numeric(x))
+groupByMinorDeletionDf= groupByResultDeletionDf[groupByResultDeletionDf ['totalMinorDeletions'] <300]
+result4 = pd.merge(result3, groupByMinorDeletionDf, how='left',  on=['week_start_utc']);
+#-------------------------------------------------
+# find total files changed  in a week by a user
+groupByResultChangeFilesDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalChangedFiles = X.changed_files.sum())
+      );
+
+result5 = pd.merge(result4, groupByResultChangeFilesDf, how='left',  on=['week_start_utc']);
+#---------------------------------------------------
+# find total number of pull request created in a week by a user
+groupByResultPullRequestsCreatedDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalPullRequestsCreated = X.pull_created_at.count())
+      );
+
+result6 = pd.merge(result5, groupByResultPullRequestsCreatedDf, how='left',  on=['week_start_utc']);
+#-------------------------------------------------
+# find total number of pull request merged in a week by a user
+groupByResultPullRequestsMergedDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalPullRequestsMerged = X.pull_merged_at.count())
+      );
+
+result7 = pd.merge(result6, groupByResultPullRequestsMergedDf, how='left',  on=['week_start_utc']);
+#-------------------------------------------------
+# find total number of pull request review closed in a week by a user
+groupByResultPullRequestClosedDf = (filteredUserData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalPullRequestsClosed = X.pull_closed_at.count())
+      );
+
+result8 = pd.merge(result7, groupByResultChangeFilesDf, how='left',  on=['week_start_utc']);
+#-------------------------------------------------
+# filter information from issue_user_bulk collection for a specific user
+collectionDf = issuesUserBulkDf ;
+collectionUserFieldName = 'issuer_id' ;
+operationFieldName = 'issue_created_at';
+filteredIssueData = tableOperation(collectionDf,collectionUserFieldName,userId,operationFieldName,firstDurationStartEp,lastDurationEndEp);
+#-----------------------------------------------------------------------------------------------------------------------------------
+# find total number of issues created in a week by a user
+groupByResultIssuesInWeekDf = (filteredIssueData >>
+        group_by(X.week_start_utc)>>
+         summarize(totalIssuesInWeek = X.issue_created_at.count())
+      );
+
+result9 = pd.merge(result8, groupByResultIssuesInWeekDf, how='left',  on=['week_start_utc']);
+#------------------------------------------------------------------------------------------------------------
+# find total number of issues created by a user in a week  and that which are assigned 
+IssueWithAssignessDf = filteredIssueData[filteredIssueData.assignees_ids.notnull()];
+groupByResultAssignedIssuesInWeekDf = (IssueWithAssignessDf >>
+        group_by(X.week_start_utc)>>
+         summarize(totalAssignedIssuesInWeek = X.issue_closed_at.count())
+      );
+
+result10 = pd.merge(result9, groupByResultAssignedIssuesInWeekDf, how='left',  on=['week_start_utc']);
+#------------------------------------------------------------------------------------------------------------
+# find total number of issues created by a user in a week  and that which are not  assigned
+#IssueWithNoAssigneesDf = filteredIssueData[filteredIssueData.assignees_ids.isnull()];
+#groupByResultNoAssigneeIssuesInWeekDf = (IssueWithNoAssigneesDf >>
+#        group_by(X.week_start_utc)>>
+#         summarize(totalNotAssignedIssuesInWeek = X.issue_closed_at.count())
+#      );
+#
+#result11 = pd.merge(result10, groupByResultNoAssigneeIssuesInWeekDf, how='left',  on=['week_start_utc']);
+##
+
+
+
